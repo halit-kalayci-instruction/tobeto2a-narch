f9c5f1799b8a1fd016b58bf32fc302e0bf4afbb2
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-04-22 09:55:27 +0300

    Messages

--- src/tobeto2A.RentACar/Application/Features/Auth/Commands/Login/LoginCommandValidator.cs
+++ src/tobeto2A.RentACar/Application/Features/Auth/Commands/Login/LoginCommandValidator.cs
@@ -1,12 +1,28 @@
-﻿using FluentValidation;
+﻿using Application.Features.Auth.Constants;
+using FluentValidation;
+using NArchitecture.Core.Localization.Abstraction;
 
 namespace Application.Features.Auth.Commands.Login;
 
 public class LoginCommandValidator : AbstractValidator<LoginCommand>
 {
-    public LoginCommandValidator()
+    private ILocalizationService _localizationService;
+    public LoginCommandValidator(ILocalizationService localizationService)
     {
-        RuleFor(c => c.UserForLoginDto.Email).NotEmpty().EmailAddress();
+        _localizationService = localizationService;
+        RuleFor(c => c.UserForLoginDto.Email).NotEmpty();
+        RuleFor(c => c.UserForLoginDto.Email).EmailAddress();
+        RuleFor(c => c.UserForLoginDto.Email).Must(MyCustomRule);
         RuleFor(c => c.UserForLoginDto.Password).NotEmpty().MinimumLength(4);
+    }
+    // 10:10
+    public bool MyCustomRule(string input)
+    {
+        return true;
+    }
+
+    public async Task<string> GetLocalized(string key)
+    {
+        return await _localizationService.GetLocalizedAsync(key, AuthMessages.SectionName);
     }
 }
--- src/tobeto2A.RentACar/Application/Features/Customers/Constants/CustomersBusinessMessages.cs
+++ src/tobeto2A.RentACar/Application/Features/Customers/Constants/CustomersBusinessMessages.cs
@@ -2,7 +2,7 @@ namespace Application.Features.Customers.Constants;
 
 public static class CustomersBusinessMessages
 {
-    public const string SectionName = "Customer";
+    public const string SectionName = "Customers";
 
     public const string CustomerNotExists = "CustomerNotExists";
 }
